3c51124ce5601d74375503e2cfcde44cc00fdcbe
harris maung <harrismaung3@gmail.com>
2021-08-26 14:38:40 +0800

    secret key update

--- FnC/settings.py
+++ FnC/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nd8_)%q%66b9vgxf$x77%0=oo729=kr(w9+b2atm^np17rxv5_'
+
+with open('/Users/harrismaung/Desktop/photoProj/FnC/FnC/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
